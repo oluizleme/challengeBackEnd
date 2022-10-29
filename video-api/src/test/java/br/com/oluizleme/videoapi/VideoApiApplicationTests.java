e8a4c0cbe81c1d5dd479e6751f4756e09d113222
oluizleme <o.luizleme@gmail.com>
2022-10-29 19:36:05 -0300

    Create VideoApiApplicationTests.java
    Classes criadas automaticamente

---
+++ video-api/src/test/java/br/com/oluizleme/videoapi/VideoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.oluizleme.videoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
